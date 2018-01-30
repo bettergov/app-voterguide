02c569b4a514b8daea095ba5b5bda5683327fd49
Patrick Judge <pjudge@bettergov.org>
2018-01-30 11:27:51 -0600

    Create election, race, candidate models

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qas.apps.QasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- project/qas/models.py
+++ project/qas/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Election(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    date = models.DateField('election date')
+
+    def __str__(self):
+        return self.title
+
+class Race(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    questions = ArrayField( models.TextField() )
+
+    def __str__(self):
+        return  self.title
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    is_incumbent = models.BooleanField()
+    PARTY_CHOICES = (
+        ('GOP', 'Republican'),
+        ('DEM', 'Democrat')
+    )
+    party = models.CharField(max_length=3, choices = PARTY_CHOICES)
+    website = models.URLField(help_text="The url for the candidate's website.", blank=True)
+    facebook = models.URLField(help_text="The url for the candidate's Facebook.", blank=True)
+    twitter = models.URLField(help_text="The url for the candidate's Twitter.", blank=True)
+    responses = ArrayField( models.TextField() )
+
+    def __str__(self):
+        return  self.name
