a2235f8318c1435dd0def4a1f83142346072d25c
Patrick Judge <pjudge@bettergov.org>
2018-02-01 15:16:32 -0600

    Redirect root url to races

--- voterguide/project/urls.py
+++ voterguide/project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('races/', include('qas.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='races/', permanent=False))
 ]
