5efd263ca02ce03e8e3926d40097ed0749381875
Patrick Judge <pjudge@bettergov.org>
2018-02-02 11:07:56 -0600

    Move around static files + references

--- voterguide/project/settings.py
+++ voterguide/project/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, "..", "..", "www", 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "..", 'static', 'app', 'css')
+    os.path.join(PROJECT_DIR, "..", '..', 'www', 'static')
 ]
 
 # CKEditor configuration
@@ -176,8 +176,8 @@ AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
 # AWS_SECRET_ACCESS_KEY = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
 
 # Tell django-storages the domain to use to refer to static files.
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected
 # static files (when you run `collectstatic`).
