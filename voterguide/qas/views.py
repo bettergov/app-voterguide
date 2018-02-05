b9312a7513970477e75d265ca16fbea82ca85894
Patrick Judge <pjudge@bettergov.org>
2018-02-05 16:16:19 -0600

    Tweaks

--- voterguide/qas/views.py
+++ voterguide/qas/views.py
@@ -5,7 +5,7 @@ from .models import Race, Candidate
 def index(request):
     all_races = Race.objects.order_by('title')
     context = { 'all_races': all_races }
-    return render(request, 'qas/index.html', context)
+    return render(request, 'qas/election.html', context)
 
 def race(request, race_slug):
     race = get_object_or_404(Race, slug=race_slug)
