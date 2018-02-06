2a0ff89fe5d4a5724405431a88efcce6d0c6ca0b
Patrick Judge <pjudge@bettergov.org>
2018-02-06 14:31:02 -0600

    Fix settings?

--- voterguide/project/settings.py
+++ voterguide/project/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, "..", "www", 'static')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, "..", "www", 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, "..", "www", 'media')
