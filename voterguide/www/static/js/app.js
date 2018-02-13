d350eccb0883bf9cd02a3f9403208ce73047d43a
Patrick Judge <pjudge@bettergov.org>
2018-02-13 13:15:16 -0600

    Refactor javascript

--- voterguide/www/static/js/app.js
+++ voterguide/www/static/js/app.js
@@ -1,15 +1,16 @@
-let appInit = function() {
-    let filters = document.getElementsByClassName('candidate-filter');
+function appInit() {
+    "use strict";
+    var filters = document.getElementsByClassName('candidate-filter');
 
-    const params = new URLSearchParams(location.search);
+    var params = new URLSearchParams(location.search);
 
     for (var i = 0; i < filters.length; i++) {
-        let f = filters[i];
-        f.addEventListener('click', function() {
-            console.log(f);
-            var key = f.dataset.candidateKey;
-            let candidates = new Set();
-            for (let v of params.values()) candidates.add(v);
+        var filter = filters[i];
+        filter.addEventListener('click', function() {
+            var key = this.dataset.candidateKey;
+
+            // Get candidates from URL
+            var candidates = new Set(params.getAll('candidate[]'));
 
             if (candidates.has(key)) {
                 candidates.delete(key);
@@ -18,25 +19,23 @@ let appInit = function() {
             }
 
             params.delete('candidate[]');
-            for (let c of candidates) {
-                params.append('candidate[]', c);
-            }
-            
+            candidates.forEach(function(c){ params.append('candidate[]', c); })
+
             window.history.replaceState({}, '', `${location.pathname}?${params}`);
             window.location.reload();
         }, false);
     };
 
 
-    let responses = document.getElementsByClassName('response__text');
+    var responses = document.getElementsByClassName('response__text');
     for (var i = 0; i < responses.length; i++) {
-        let r = responses[i];
+        var r = responses[i];
         if (r.clientHeight > 150) r.parentNode.classList.add("collapsed");
     }
 
-    let expanders = document.getElementsByClassName('response-expand');
+    var expanders = document.getElementsByClassName('response-expand');
     for (var i = 0; i < expanders.length; i++) {
-        let e = expanders[i];
+        var e = expanders[i];
         e.addEventListener('click', function() {
             e.parentNode.classList.remove("collapsed");
         }, false);
