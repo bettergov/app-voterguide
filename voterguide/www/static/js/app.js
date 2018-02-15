e01d2eecd407e582e64c316df6c3c6aaa469aad5
Patrick Judge <pjudge@bettergov.org>
2018-02-15 08:56:58 -0600

    Fix candidate selection for browsers without URLSearchParams

--- voterguide/www/static/js/app.js
+++ voterguide/www/static/js/app.js
@@ -2,30 +2,57 @@ function appInit() {
     "use strict";
     var filters = document.getElementsByClassName('candidate-filter');
 
-    var params = new URLSearchParams(location.search);
+    try {
+        var params = new URLSearchParams(location.search);
 
-    for (var i = 0; i < filters.length; i++) {
-        var filter = filters[i];
-        filter.addEventListener('click', function() {
-            var key = this.dataset.candidateKey;
+        for (var i = 0; i < filters.length; i++) {
+            var filter = filters[i];
+            filter.addEventListener('click', function() {
+                var key = this.dataset.candidateKey;
 
-            // Get candidates from URL
-            var candidates = new Set(params.getAll('candidate[]'));
+                // Get candidates from URL
+                var candidates = new Set(params.getAll('candidate[]'));
 
-            if (candidates.has(key)) {
-                candidates.delete(key);
-            } else {
-                candidates.add(key);
-            }
+                if (candidates.has(key)) {
+                    candidates.delete(key);
+                } else {
+                    candidates.add(key);
+                }
 
-            params.delete('candidate[]');
-            candidates.forEach(function(c){ params.append('candidate[]', c); })
+                params.delete('candidate[]');
+                candidates.forEach(function(c){ params.append('candidate[]', c); })
 
-            window.history.replaceState({}, '', `${location.pathname}?${params}`);
-            window.location.reload();
-        }, false);
-    };
+                window.history.replaceState({}, '', `${location.pathname}?${params}`);
+                window.location.reload();
+            }, false);
+        };
+    } catch(error) {
+        var string = location.search;
+        var reg = /[0-9]+/g;
+        var matches = string.match(reg);
 
+        for (var i = 0; i < filters.length; i++) {
+            var filter = filters[i];
+            filter.addEventListener('click', function() {
+                var key = this.dataset.candidateKey;
+
+                // Get candidates from URL
+                var candidates = new Set(matches);
+
+                if (candidates.has(key)) {
+                    candidates.delete(key);
+                } else {
+                    candidates.add(key);
+                }
+
+                var params = "";
+                candidates.forEach(function(d){ params += 'candidate[]=' + d + '&'});
+
+                window.history.replaceState({}, '', `${location.pathname}?${params}`);
+                window.location.reload();
+            }, false);
+        };
+    }
 
     var responses = document.getElementsByClassName('response__text');
     for (var i = 0; i < responses.length; i++) {
